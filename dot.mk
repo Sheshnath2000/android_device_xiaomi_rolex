09140e683deabf534ddb81b331b330bdfaa1e56c
sheshnath maurya <sheshnathmaurya2000@gmail.com>
2018-10-17 12:08:26 +0000

    dot_oreo

--- dot.mk
+++ dot.mk
@@ -14,17 +14,29 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/xiaomi/rolex/full_rolex.mk)
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, device/xiaomi/rolex/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/dot/config/common.mk)
 
-PRODUCT_NAME := lineage_rolex
-BOARD_VENDOR := Xiaomi
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := rolex
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi 4A
+PRODUCT_NAME := dot_rolex
+
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+TARGET_VENDOR_PRODUCT_NAME := rolex
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="rolex" \
+    PRODUCT_NAME="rolex" \
     PRIVATE_BUILD_DESC="rolex-user 7.1.2 N2G47H V9.2.6.0.NCCMIEK release-keys"
 
 # Set BUILD_FINGERPRINT variable
