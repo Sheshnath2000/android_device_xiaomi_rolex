18fe37eda20e8cd54fd29332c81b3959f2bc37a7
sheshnath maurya <sheshnathmaurya2000@gmail.com>
2019-01-26 07:18:35 +0000

    aexo

--- aosp_rolex.mk
+++ aosp_rolex.mk
@@ -14,19 +14,34 @@
 # limitations under the License.
 #
 
-$(call inherit-product, device/xiaomi/rolex/full_rolex.mk)
+#Bootanimation res
+TARGET_BOOT_ANIMATION_RES := 720
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+$(call inherit-product, device/xiaomi/rolex/device.mk)
 
 # Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/aosp/common.mk)
 
-PRODUCT_NAME := lineage_rolex
-BOARD_VENDOR := Xiaomi
+
+PRODUCT_BRAND := Xiaomi
+PRODUCT_DEVICE := rolex
+PRODUCT_MANUFACTURER := Xiaomi
+PRODUCT_MODEL := Redmi 4A
+PRODUCT_NAME := aosp_rolex
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
+TARGET_VENDOR_PRODUCT_NAME := rolex
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
+    TARGET_DEVICE="rolex" \
+    PRODUCT_NAME="rolex" \
     PRIVATE_BUILD_DESC="rolex-user 7.1.2 N2G47H V9.2.6.0.NCCMIEK release-keys"
 
-# Set BUILD_FINGERPRINT variable
+# Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := "Xiaomi/rolex/rolex:7.1.2/N2G47H/V9.2.6.0.NCCMIEK:user/release-keys"
 
